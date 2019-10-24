3d24a9e0c32711df3a2e89ddc1d635c078b41e47
vilnytskyy <vilnytskyy21@gmail.com>
2019-10-24 10:57:14 -0400

    added code function removeLeadingSpaces()

--- funcs.cpp
+++ funcs.cpp
@@ -1,1 +1,27 @@
 #include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+//task a
+std::string removeLeadingSpaces(std::string line)
+{
+    std::string result = "";
+
+    //std::vector<int> pos;
+
+    bool nspace = true;
+
+    for(int i=0; i<line.size(); i++){
+        if(nspace&&!isspace(line[i])){
+            result+=line[i];
+            nspace = false;
+        }else if(line[i]=='\n'){
+            result+=line[i];
+            nspace =true;
+        }else if(!nspace){
+            result+=line[i];
+        }
+    }
+
+    return result;
+}
