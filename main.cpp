6e3894963caf4e984913dd6c2129881be7496b11
vilnytskyy <vilnytskyy21@gmail.com>
2019-10-24 11:00:51 -0400

    added call for removeLeadingSpaces()

--- main.cpp
+++ main.cpp
@@ -4,5 +4,5 @@
 #include "funcs.h"
 
 int main(){
-    //code
+    std::cout << removeLeadingSpaces("                int main(){\n           // Hi, I'm a program!\nint x = 1;\n    for(int i = 0; i < 10; i++) {\ncout << i;\n          cout << endl;\n }\n    }") << std::endl;
 }
