4db91f2f263d58f7e324811b72130c31bb732733
vilnytskyy <vilnytskyy21@gmail.com>
2019-10-28 00:55:46 -0400

    added task b function paramaters

--- funcs.h
+++ funcs.h
@@ -1,4 +1,10 @@
 #pragma once
-#include <string>
+//#include <string>
 
 std::string removeLeadingSpaces(std::string);
+
+int countChar(std::string, char);
+
+std::string indent(std::string, int);
+
+std::string format(std::string);
