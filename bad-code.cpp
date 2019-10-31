32b4e6405f39c89f2b7d5f1988e897aa08493afc
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2019-10-31 11:00:51 -0400

    added gitignore and works with brackets in single line quotes

--- bad-code.cpp
+++ bad-code.cpp
@@ -1,6 +1,7 @@
                 int main(){
            // Hi, I'm a program!
-int x = 1; 
+int x = 1;
+std::string str = "e{";
     for(int i = 0; i < 10; i++) {
 cout << i;
           cout << endl;
